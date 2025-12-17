c98d6ffcc4aea2f2176e2477954318745a253aac
osigie <kenosagie88@gmail.com>
2025-12-17 17:04:07 +0100

    setup

---
+++ src/test/java/com/osigie/eazi_wallet/EaziWalletApplicationTests.java
@@ -0,0 +1,13 @@
+package com.osigie.eazi_wallet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaziWalletApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
