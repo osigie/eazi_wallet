76a1ebab8255559ac853043205fb1ef9372df947
osigie <kenosagie88@gmail.com>
2025-12-18 10:48:53 +0100

    feat(config): add abstract class for postgres and env setup

--- src/test/java/com/osigie/eazi_wallet/config/AbstractContainerBaseTest.java
+++ src/test/java/com/osigie/eazi_wallet/config/AbstractContainerBaseTest.java
@@ -1,26 +1,38 @@
-package com.osigie.springunitintegration.controller;
+package com.osigie.eazi_wallet.config;
 
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
-import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
 
 public abstract class AbstractContainerBaseTest {
 
-    static final MySQLContainer MY_SQL_CONTAINER;
+
+    static final PostgreSQLContainer MY_POSTGRES_CONTAINER;
 
     static {
-        MY_SQL_CONTAINER = new MySQLContainer("mysql:latest")
-                .withUsername("username")
-                .withPassword("password")
-                .withDatabaseName("org_db");
 
-        MY_SQL_CONTAINER.start();
+        MY_POSTGRES_CONTAINER = new PostgreSQLContainer<>(DockerImageName.parse("postgres:16-alpine"))
+                .withDatabaseName("eazi_wallet")
+                .withUsername("username")
+                .withPassword("password");
+
+
+        MY_POSTGRES_CONTAINER.start();
     }
 
     @DynamicPropertySource
     public static void dynamicPropertySource(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", MY_SQL_CONTAINER::getJdbcUrl);
-        registry.add("spring.datasource.username", MY_SQL_CONTAINER::getUsername);
-        registry.add("spring.datasource.password", MY_SQL_CONTAINER::getPassword);
+        registry.add("spring.datasource.url", MY_POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.datasource.username", MY_POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.datasource.password", MY_POSTGRES_CONTAINER::getPassword);
+
+        registry.add("spring.flyway.enabled", () -> true);
+        registry.add("spring.flyway.url", MY_POSTGRES_CONTAINER::getJdbcUrl);
+        registry.add("spring.flyway.user", MY_POSTGRES_CONTAINER::getUsername);
+        registry.add("spring.flyway.password", MY_POSTGRES_CONTAINER::getPassword);
+
+        // Hibernate configuration
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "validate");
     }
 }
